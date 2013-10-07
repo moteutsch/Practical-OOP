02e5ad8ea1f190e7e3de6863cdc2cf253f60cc8d
Moshe Teutsch <moteutsch@gmail.com>
2013-10-07 14:04:07 +0300

    Remove unneeded bits

--- code/index.php
+++ code/index.php
@@ -1,13 +1,9 @@
 <?php
 
-ini_set('display_errors', true);
-error_reporting(-1);
-
 require 'vendor/autoload.php';
 
 session_start();
 $service = new \QuizApp\Service\Quiz(
-    //new \QuizApp\Mapper\HardCoded()
     new \QuizApp\Mapper\Mongo((new \MongoClient)->practicaloop->quizes)
 );
 
@@ -23,20 +19,15 @@ $app->get('/choose-quiz/:id', function ($id) use ($service, $app) {
     $app->redirect('/solve-question');
 });
 $app->get('/solve-question', function () use ($service, $app) {
-    $wasAnswerCorrect = isset($_SESSION['wasAnswerCorrect']) ? $_SESSION['wasAnswerCorrect'] : null;
-    unset($_SESSION['wasAnswerCorrect']);
     $app->render('solve-question.phtml', array(
         'question' => $service->getQuestion(),
-        'wasAnswerCorrect' => $wasAnswerCorrect,
     ));
 });
 $app->post('/check-answer', function () use ($service, $app) {
     $isCorrect = $service->checkSolution($app->request->post('id'));
-    $_SESSION['wasAnswerCorrect'] = $isCorrect;
     if (!$service->isOver()) {
         $app->redirect('/solve-question');
     } else {
-        unset($_SESSION['wasAnswerCorrect']);
         $app->redirect('/end');
     }
 });
